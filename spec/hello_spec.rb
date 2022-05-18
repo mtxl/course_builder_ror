a8ad8d821f64f9c146811295abfe19645a2c62f2
Yuri Beloborodov <yubeloborodov@gmail.com>
2022-05-18 21:52:23 +0500

    Fix 2_13

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe "hello spec" do
   describe "math" do
     it "should be able to perform basic math" do
-      # expect(6 * 7).to eq(43) # => false
-      expect(6 * 7).to eq(42)
+      expect(6 * 7).to eq(43) # => false
+      # expect(6 * 7).to eq(42)
     end
   end
 end
