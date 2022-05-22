e1befbf5fe8209f9443c1ca018e56c34e59f59fa
evsvmx <makss.web@gmail.com>
2022-05-22 18:57:35 +0300

    added to CoursesController #create and #update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'courses', to: 'courses#index'
+  get 'courses/new', to: 'courses#new'
+  post 'courses', to: 'courses#create'
+  get 'courses/:id/edit', to: 'courses#edit'
+  put 'courses/:id', to: 'courses#update'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "courses#index"
 end
