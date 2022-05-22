54a84841863a87b0338265a2a9c726159f8e428c
evsvmx <makss.web@gmail.com>
2022-05-22 18:57:35 +0300

    added to CoursesController #create and #update

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,4 +2,39 @@ class CoursesController < ApplicationController
   def index
     @courses = Course.all
   end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.create(course_params)
+
+    if @course.save
+      redirect_to action: 'index'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+    redirect_to action: 'index' if @course.nil?
+  end
+
+  def update
+    @course = Course.find(params[:id])
+
+    if @course.update(course_params)
+      redirect_to action: 'index'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :volume, :price)
+  end
 end
