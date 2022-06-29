cab136e11a669b0d720cf0b68cfa1068256d4f71
MinasMorgulNazgul <razborov-aa@yandex.ru>
2022-06-29 21:02:57 +0300

    add Devise and model User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'courses', to: 'courses#index'
   get 'courses/new', to: 'courses#new'
   post 'courses', to: 'courses#create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_22_094823) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_29_173852) do
   create_table "courses", force: :cascade do |t|
     t.string "title"
     t.string "volume"
@@ -29,6 +29,20 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_22_094823) do
     t.integer "course_id", null: false
     t.index ["course_id"], name: "index_units_courses_on_course_id"
     t.index ["unit_id"], name: "index_units_courses_on_unit_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "units_courses", "courses"
