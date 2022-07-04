fd85e0794763a773bc24310e8436df70a7bf112c
AndreyChim <chimitcyrenov@mail.ru>
2022-07-04 15:14:35 +0800

    proba

--- app/components/table_component.rb
+++ app/components/table_component.rb
@@ -1,9 +1,9 @@
 class TableComponent < ViewComponent::Base
-    def initialize(header:, body:, row:, footer:)
-      @header = header
-      @body = body
-      @row = row
-      @footer = footer
+    def initialize(title:, volume:, price:)
+      @title = title
+      @volume = volume
+      @price = price
+    
     end
   
   end
